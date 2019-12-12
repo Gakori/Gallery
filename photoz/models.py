ce904300cb17bf09ca07497c92d6a189b7d641af
faith gakori <faithgakori506@gmail.com>
2019-12-12 13:15:56 +0300

    create location and category class

--- photoz/models.py
+++ photoz/models.py
@@ -5,7 +5,21 @@ class Image(models.Model):
     image_description = models.TextField(max_length=100)
     image_category = models.CharField(max_length=30)
     image_location = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     
     def __str__(self):
         return self.image_title
+
+class Category(models.Model):
+    title = models.CharField(max_length=30),
+    
+    def __str__(self):
+        return f'name: {self.name}'
+    
+    
+class Location(models.Model):
+    place = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return f'place: {self.place}'
+    
