63bd3c952571ed50e3fb8c81d6621f800ce01cd0
faith gakori <faithgakori506@gmail.com>
2019-12-13 12:20:04 +0300

    create a url to display image credentials

--- photoz/urls.py
+++ photoz/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.welcome,name ='welcome'),
     url(r'^search/',views.search_results,name='search_results'),
-    # url(r'^image/(/d+)',views.image,name='image')
+    url(r'^image/(\d+)',views.image,name='image')
 ]
 
 if settings.DEBUG:
